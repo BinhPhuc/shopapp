d4f6fe62b75bb9db09a7402e5ff98c3a09a1a70f
BinhPhuc <phamphucbinh_hsgs2021@hus.edu.vn>
2024-02-10 18:04:28 +0700

    handle exception

---
+++ src/main/java/com/project/shopapp/exception/InvalidParamException.java
@@ -0,0 +1,7 @@
+package com.project.shopapp.exception;
+
+public class InvalidParamException extends Exception{
+    public InvalidParamException(String message) {
+        super(message);
+    }
+}
