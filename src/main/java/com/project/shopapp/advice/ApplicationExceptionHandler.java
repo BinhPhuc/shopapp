555cd01596e5900a96a9a71779a6f7a59734f17a
BinhPhuc <phamphucbinh_hsgs2021@hus.edu.vn>
2024-03-26 09:50:07 +0700

    handle new exception

--- src/main/java/com/project/shopapp/advice/ApplicationExceptionHandler.java
+++ src/main/java/com/project/shopapp/advice/ApplicationExceptionHandler.java
@@ -90,4 +90,12 @@ public class ApplicationExceptionHandler {
         errorMap.put("errorMessage", ex.getMessage());
         return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(ExistDataException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<?> handlePermissionDenied (ExistDataException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("errorMessage", ex.getMessage());
+        return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+    }
 }
