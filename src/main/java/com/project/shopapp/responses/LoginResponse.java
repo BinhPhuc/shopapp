d2c368de859818472c07bd3efe94f11a99ee5eda
BinhPhuc <phamphucbinh_hsgs2021@hus.edu.vn>
2024-03-26 09:49:41 +0700

    adding login respone for json parse

---
+++ src/main/java/com/project/shopapp/responses/LoginResponse.java
@@ -0,0 +1,19 @@
+package com.project.shopapp.responses;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class LoginResponse {
+    @JsonProperty("message")
+    private String message;
+
+    @JsonProperty("token")
+    private String token;
+}
