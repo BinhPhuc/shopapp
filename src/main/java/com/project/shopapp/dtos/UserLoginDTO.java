ba9d03e11bd265e4407dbdbee7b2da224d22540d
BinhPhuc <phamphucbinh_hsgs2021@hus.edu.vn>
2024-03-29 23:30:58 +0700

    adding role_id property

--- src/main/java/com/project/shopapp/dtos/UserLoginDTO.java
+++ src/main/java/com/project/shopapp/dtos/UserLoginDTO.java
@@ -1,6 +1,7 @@
 package com.project.shopapp.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
@@ -19,4 +20,8 @@ public class UserLoginDTO {
 
     @NotEmpty(message = "Password is required!")
     private String password;
+
+    @Min(value = 1, message = "You must enter role's id!")
+    @JsonProperty("role_id")
+    private Long roleId;
 }
